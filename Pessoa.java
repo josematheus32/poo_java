ff48af4437663766d9fd5ddfbf0f4758fdc81716
José Matheus <144465858+josematheus32@users.noreply.github.com>
2024-02-28 10:45:44 -0300

    Classes e Encapsulamento

--- Pessoa.java
+++ Pessoa.java
@@ -1,60 +1,35 @@
-public class Pessoa{
-        
+public class Pessoa {
+    
     private String nome;
     private int idade;
-    private String sexo;
-    private String corOlhos;
-    private String corCabelos;
-    private double altura;
+    private String endereco;
 
-    public Pessoa(String nome, int idade, String sexo, String corOlhos, String corCabelos, double altura){
+        
+    public Pessoa(String nome, int idade, String endereco) {
         this.nome = nome;
         this.idade = idade;
-        this.sexo = sexo;
-        this.corOlhos = corOlhos;
-        this.corCabelos = corCabelos;
-        this.altura = altura;
+        this.endereco = endereco;
     }
 
-    public Pessoa(){
-        
-    }
-
-    public String getNome(String nome){
+    
+    public String getNome() {
         return nome;
-    } 
-    public void setNome(String nome){
+    }
+    public void setNome(String nome) {
         this.nome = nome;
     }
-    public int getIdade(int idade){
+    public int getIdade() {
         return idade;
     }
-    public void setIdade(int idade){
+    public void setIdade(int idade) {
         this.idade = idade;
     }
-    public String getSexo(String sexo){
-        return sexo;
-    } 
-    public void setSexo(String sexo){
-        this.sexo = sexo;
+    public String getEndereco() {
+        return endereco;
     }
-    public String getCorOlhos(String corOlhos){
-        return corOlhos;
-    } 
-    public void setCorOlhos(String corOlhos){
-        this.corOlhos = corOlhos;
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
     }
-    public String getCorCabelos(String corCabelos){
-        return corCabelos;
-    } 
-    public void setCorCabelos(String corCabelos){
-        this.corCabelos = corCabelos;
-    }
-    public double getAltura(double altura){
-        return altura;
-    } 
-    public void setAltura(double altura){
-        this.altura = altura;
-    }
+
     
 }
